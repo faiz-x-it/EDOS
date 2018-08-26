c20ccb9853cd56dc8953f3515ba704b7e15ba311
EduApps-CDG <eduardoprocopiogomez@gmail.com>
2018-08-26 00:36:38 -0300

    Adicionado o FastSettings, inspirado no Android!

--- @SYS/htmlos.js
+++ @SYS/htmlos.js
@@ -1,11 +1,38 @@
-window.onload = new function() {
-		window.moveTo(0, 1);
-		if (document.all) {
-				top.window.resizeTo(screen.availWidth, screen.availHeight);
-		} else if (document.layers || document.getElementById) {
-				if (top.window.outerHeight < screen.availHeight || top.window.outerWidth < screen.availWidth) {
-						top.window.outerHeight = screen.availHeight;
-						top.window.outerWidth = screen.availWidth;
-				}
-		}
+//document vars
+var settings = {
+	fastsettings: {
+		enabled: 1,
+	}
 };
+var desktop_item = document.getElementsByClassName("item");
+var fastsettings_popup = document.getElementById("fastsettingspopup");
+//fastsettings_popup.style.visibility = "hidden";
+
+//DesktopItem worker
+desktop_item.onmousedown = new function() {
+	document.getElementById('trash').style.backgroundColor = 'rgba(0,74,255,0.7)';
+};
+desktop_item.onmouseup = new function() {
+	document.getElementById('trash').style.backgroundColor = 'rgba(0,74,255,0.0)';
+};
+desktop_item.ontouchstart = new function() {
+	document.getElementById('trash').style.backgroundColor = 'rgba(0,74,255,0.7)';
+};
+desktop_item.ontouchend = new function() {
+	document.getElementById('trash').style.backgroundColor = 'rgba(0,74,255,0.0)';
+};
+fastsettings_popup.style.visibility = "hidden";
+
+//FastSettings ToolBar Button
+function fastsettings() {
+	var fastsettings = document.getElementById("fastsettings");
+	if (settings.fastsettings.enabled == 1) {
+		fastsettings_popup.style.visibility = "hidden";
+		fastsettings.src = "/HTML-OS/@APP/htmlos.system.ui/icons/fastsettings-disabled.png";
+		settings.fastsettings.enabled++;
+	} else if (settings.fastsettings.enabled == 2) {
+		fastsettings_popup.style.visibility = "visible";
+		fastsettings.src = "/HTML-OS/@APP/htmlos.system.ui/icons/fastsettings-enabled.png";
+		settings.fastsettings.enabled = 1;
+	}
+}
--- pwabuilder-sw-register.js
+++ pwabuilder-sw-register.js
@@ -2,11 +2,11 @@
 
 //Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
 if (navigator.serviceWorker.controller) {
-  console.log('[PWA Builder] active service worker found, no need to register')
+  console.log('[PWA Builder] active service worker found, no need to register');
 } else {
 
 //Register the ServiceWorker
-  navigator.serviceWorker.register('pwabuilder-sw.js', {
+  navigator.serviceWorker.register('workbox-sw.prod.v2.1.0.js', {
     scope: './'
   }).then(function(reg) {
     console.log('Service worker has been registered for scope:'+ reg.scope);
