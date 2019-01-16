c5fe68c5d29def48d8fb659963e7a9fb79049e57
EduApps-CDG <eduardoprocopiogomez@gmail.com>
2019-01-15 23:36:19 -0200

    f*** off

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,7 @@
-//Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
 if (navigator.serviceWorker.controller) {
   console.log('[PWA Builder] active service worker found, no need to register')
 } else {
 
-//Register the ServiceWorker
   navigator.serviceWorker.register('pwabuilder-sw.js', {
     scope: './'
   }).then(function(reg) {
@@ -16,7 +14,6 @@ window.addEventListener('beforeinstallprompt', function(e) {
   console.log('beforeinstallprompt Event fired');
   e.preventDefault();
 
-  // Stash the event so it can be triggered later.
   deferredPrompt = e;
 
   return false;
@@ -24,11 +21,8 @@ window.addEventListener('beforeinstallprompt', function(e) {
 
 btnSave.addEventListener('click', function() {
   if(deferredPrompt !== undefined) {
-    // The user has had a postive interaction with our app and Chrome
-    // has tried to prompt previously, so let's show the prompt.
     deferredPrompt.prompt();
 
-    // Follow what the user has done with the prompt.
     deferredPrompt.userChoice.then(function(choiceResult) {
 
       console.log(choiceResult.outcome);
@@ -39,8 +33,7 @@ btnSave.addEventListener('click', function() {
       else {
         console.log('User added to home screen');
       }
-
-      // We no longer need the prompt.  Clear it up.
+	  
       deferredPrompt = null;
     });
   }
